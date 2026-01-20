0c51dc3ef2c81c2f93f6c59aa686ad9facfd43f4
Oleksandr_Kotenko <oleksandr.kotenko@outlook.com>
2026-01-20 17:41:12 +0100

    Create a program to print ascii art into console

---
+++ courses/coding-in-C/Lab_2/my-solution/asciiworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("  /^\\\n");
+    printf("  |#|\n");
+    printf(" |===|\n");
+    printf("  |0|\n");
+    printf("  | |\n");
+    printf(" =====\n");
+    printf("_||_||_\n");
+    return 0;
+}
