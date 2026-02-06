287d137f0223fd465f498d2efb895ac85e466473
Oleksandr_Kotenko <oleksandr.kotenko@outlook.com>
2026-02-06 13:05:58 +0100

    Add ask_for_operation

--- courses/coding-in-C/Lab_3/my-solution/calculator.c
+++ courses/coding-in-C/Lab_3/my-solution/calculator.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef enum {
+    ADD = 0,
+    MIN,
+    MULT,
+    DIV
+} Operation;
+
 float ask_for_number(unsigned short n) {
     float number;
     char c;
@@ -17,9 +24,28 @@ float ask_for_number(unsigned short n) {
     return number;
 }
 
-int main () {
-    while (true) {
-        printf("%f", ask_for_number(1));
+short ask_for_operation() {
+    char c;
+    printf("Please enter operation (+, -, * or /): ");
+    c = getchar();
+    switch (c) {
+    case '+':
+        return ADD;
+    case '-':
+        return MIN;
+    case '*':
+        return MULT;
+    case '/':
+        return DIV;
+    default:
+        printf("Something went wrong. Please try again.\n");
+        return ask_for_operation();
+        break;
     }
+    while ((c = getchar()) != '\n' && c != EOF); // reading and cleaning the wrong input line    
+}
+
+int main () {
+    printf("%i",ask_for_operation());
     return 1;
 }
